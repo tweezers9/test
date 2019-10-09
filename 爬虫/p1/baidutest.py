eaf682f8b3eac880f74fac34eb2877a08345e3ca
ffcyd <2896174248@qq.com>
2019-10-09 11:18:56 +0800

    123

--- 爬虫/p1/baidutest.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-
-url = "https://www.baidu.com/"
-response = urllib.request.urlopen(url)
-html_string = response.read().decode("utf-8")
-print(html_string)
-with open("baidu1.html","w",encoding="utf-8") as fp:
-    fp.write(html_string)
--- 爬虫/p1/urlib基础.py
+++
@@ -1,4 +0,0 @@
-import urllib.request
-import urllib.parse
-
-1.定位有效URL，URL请求地址
